97f2d608ea89ebc467750cb163fa1c89709dd49f
Heather-Marsh <103526414+Heather-Marsh@users.noreply.github.com>
2024-01-21 22:52:44 -0500

    Upload Overleaf

---
+++ EC 0.6 example.py
@@ -0,0 +1,3 @@
+thistuple = ("dog", "cat", "horse","lion", "tiger", "snake","cow", "wolf","bear", "koala", "blobfish","giraffe", "capybara","deer", "turtle", "axolotl","frog", "quokka","shark", "leopard", "otter")
+if "bear" in thistuple:
+  print("Yes, 'bear' is in the animals tuple")
